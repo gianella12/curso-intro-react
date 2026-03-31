1b8378219d11802a96ca66c212cf1e843691cea8
gianella12 <gianellalastra4@gmail.com>
2026-03-31 15:07:39 -0300

    migracion de useState a useReducer

--- src/TodoContext/useLocalStorage.js
+++ src/TodoContext/useLocalStorage.js
@@ -1,40 +1,84 @@
-import { useEffect, useState } from "react";
+import { useEffect, useReducer } from "react";
 
 function useLocalStorage(itemName, initialValue) {
-    const [item, setItem] = useState(initialValue)
-    const [loading, setLoading] = useState(true)
-    const [error, setError] = useState(false)
+  const [state, dispatch] = useReducer(
+    reducer,
+    initialState({ initialValue }),
+  );
+  const { error, loading, item } = state;
 
+  //LLAMAR A LOS ACTIONS CREATORS
+  const onError = (error) =>
+    dispatch({ type: actionType.error, payload: error });
 
-    useEffect(() => {
-        setTimeout(() => {
-            try {
-                const localStorageItem = localStorage.getItem(itemName);
+  const onSuccess = (parsedItems) =>
+    dispatch({ type: actionType.success, payload: parsedItems });
 
-                let parsedItems;
+  const onSave = (item) => 
+    dispatch({ type: actionType.save, payload: item })
 
-                if (!localStorageItem) {
-                    localStorage.setItem(itemName, JSON.stringify(initialValue));
-                    parsedItems = initialValue;
+  useEffect(() => {
+    setTimeout(() => {
+      try {
+        const localStorageItem = localStorage.getItem(itemName);
 
-                } else {
-                    parsedItems = JSON.parse(localStorageItem);
-                    setItem(parsedItems);
-                }
+        let parsedItems;
 
-                setLoading(false);
-            } catch (error) {
-                setLoading(false);
-                setError(true);
-            }
-        }, 2000);
-    },[]);
+        if (!localStorageItem) {
+          localStorage.setItem(itemName, JSON.stringify(initialValue));
+          parsedItems = initialValue;
+        } else {
+          parsedItems = JSON.parse(localStorageItem);
+        }
 
+        onSuccess(parsedItems)
 
-    const saveItem = (newItem) => {
-        localStorage.setItem('TODOS_V1', JSON.stringify(newItem));
-        setItem(newItem);
+      } catch (error) {
+        onError(error);
+      }
+    }, 2000);
+  }, []);
+
+  const saveItem = (newItem) => {
+    try {
+      localStorage.setItem("TODOS_V1", JSON.stringify(newItem));
+      onSave(newItem);
+    } catch (error) {
+      onError(error);
     }
-    return { item, saveItem, loading, error };
+  };
+  return { item, saveItem, loading, error };
 }
+const initialState = ({ initialValue }) => ({
+  error: false,
+  loading: true,
+  item: initialValue,
+});
+
+const actionType = {
+  error: "ERROR",
+  success: "SUCCESS",
+  save: "SAVE",
+};
+
+const reducerObject = (state, payload) => ({
+  [actionType.error]: {
+    ...state,
+    error: true,
+  },
+  [actionType.success]: {
+    ...state,
+    error: false,
+    loading: false,
+    item: payload,
+  },
+   [actionType.save]: {
+    ...state,
+    item: payload,
+  }
+});
+
+const reducer = (state, action) => {
+  return reducerObject(state, action.payload)[action.type] || state;
+};
 export { useLocalStorage };
