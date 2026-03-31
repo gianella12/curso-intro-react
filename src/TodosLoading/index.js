e631ecd90ae3beef5407a239f451309e399de923
gianella12 <gianellalastra4@gmail.com>
2026-03-31 16:26:15 -0300

    cambios sutiles en los estilos

--- src/TodosLoading/index.js
+++ src/TodosLoading/index.js
@@ -2,7 +2,8 @@ import "./todosLoading.css"
 function TodosLoading() {
     return (
         <div className="loading-container">
-            <p>Cargando...</p>
+            <div className="pelota"/>
+            <div className="pelota"/>
             <div className="pelota"/>
         </div>
     )
